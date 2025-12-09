cea383ab5913a212b0a544e6f27ca6481e4e704f
Krish Rastogi <krishras23@gmail.com>
2025-12-09 00:20:28 -0500

    bfs implementation

---
+++ src/main/java/graph/TreeNode.java
@@ -0,0 +1,12 @@
+package graph;
+
+public class TreeNode<T>{
+  public T val;
+  public TreeNode<T> left;
+  public TreeNode<T> right;
+  public TreeNode(T val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
---
+++ src/main/java/graph/bfs.java
@@ -0,0 +1,62 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class bfs {
+  public List<Integer> traverse(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    if (root == null){
+      return result;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+
+    while(!queue.isEmpty()){
+      TreeNode node = queue.poll();
+      result.add((Integer) node.val);
+      if (node.left != null){
+        queue.add(node.left);
+      }
+      if (node.right != null){
+        queue.add(node.right);
+      }
+    }
+
+    return result;
+  }
+
+  public List<List<Integer>> traverseByLevel(TreeNode root) {
+    List<List<Integer>> result = new ArrayList<>();
+    if (root == null){
+      return result;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+
+    while(!queue.isEmpty()){
+      int size = queue.size();
+      List<Integer> level = new ArrayList<>();
+
+      for (int i = 0; i < size; i++) {
+        TreeNode node = queue.poll();
+        level.add((Integer) node.val);
+
+        if (node.left != null){
+          queue.add(node.left);
+        }
+
+        if (node.right != null){
+          queue.add(node.right);
+        }
+      }
+      result.add(level);
+    }
+    return result;
+  }
+}
